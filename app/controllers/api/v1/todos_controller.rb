0caa35a716e9ea144504fa01a9ffb0bcee91674f
ManuelCabral98 <espinola.alejandro09@gmail.com>
2026-06-08 22:30:08 -0300

    API REST completed with CRUD endpoints

--- app/controllers/api/v1/todos_controller.rb
+++ app/controllers/api/v1/todos_controller.rb
@@ -1,7 +1,44 @@
 class Api::V1::TodosController < ApplicationController
+    skip_before_action :verify_authenticity_token
+
     # GET method
     def index
         @todos = Todo.all
         render json: @todos, include: :category
+    end
+
+    # POST method
+    def create
+        @todo = Todo.new(todo_params)
+
+        if @todo.save
+            render json: @todo, include: :category, status: :created
+        else
+            render json: { errors: @todo.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+
+    # PATCH/PUT method
+    def update
+        @todo = Todo.find(params[:id])
+
+        if @todo.update(todo_params)
+            render json: @todo, include: :category
+        else
+            render json: { errors: @todo.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+
+    # DELETE method
+    def destroy
+        @todo = Todo.find(params[:id])
+        @todo.destroy
+        head :no_content # returns 204 state without text
+    end
+
+    # strong parameters for security. This avoids external injection into the database
+    private
+    def todo_params
+        params.require(:todo).permit(:title, :completed, :category_id)
     end
 end
