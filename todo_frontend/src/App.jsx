7c5873143881ecdfb9615d5b649635f765b5b066
ManuelCabral98 <espinola.alejandro09@gmail.com>
2026-06-17 17:07:42 -0300

    feat: implement task creation and fetching from rails API

--- todo_frontend/src/App.jsx
+++ todo_frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { useState,useEffect } from 'react'
 
 function App() {
   const [todos, setTodos] = useState([])
+  const [newTodoTitle, setNewTodoTitle] = useState("")
 
   useEffect(() => {
     fetch("http://localhost:3000/api/v1/todos").then(response => response.json()).then(data => {
@@ -10,6 +11,32 @@ function App() {
     })
     .catch(error => console.error("Error conectando con Rails:", error))
   }, [])
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (newTodoTitle.trim() === "") return
+
+    const bodyData ={
+      todo: {
+        title: newTodoTitle,
+        completed: false
+      }
+    }
+
+    fetch("http://localhost:3000/api/v1/todos", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(bodyData)
+    })
+    .then(response => response.json())
+    .then(newTodoCreated => { 
+      setTodos([...todos, newTodoCreated]) 
+      setNewTodoTitle("")
+    })
+    .catch(error => console.error("Error creating task: ", error))
+  }
 
   return (
     <div>
